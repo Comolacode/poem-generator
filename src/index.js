02c15a11e0170f6516e2bd5d07977d6dc9734d67
Comolacode <Jessica_g_08@yahoo.com>
2024-04-04 08:23:38 -0500

    added basic form handling and added typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generatePoem(event){
+    PerformanceEventTiming.preventDefault();
+}
+let poemElement = document.querySelector("poem");
+
+new Typewriter("#poem", {
+  strings: ["poem poem poem poem"],
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+
+let poemFormElement = document.querySelector("poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
